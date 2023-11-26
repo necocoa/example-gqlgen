50c09564b2b61b5d75240b4a6227f144ab756b63
necocoa <joyds217@gmail.com>
2023-11-26 16:46:28 +0900

    Implement the resolvers

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,12 @@
 package graph
 
+import "gqlgen_tutorial/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+// Resolver is the root resolver.
+type Resolver struct {
+	todos []*model.Todo
+}
